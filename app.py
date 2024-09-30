ca2de4ae9b832e6190877dfb34b464bb3e863891
Maria <145807015+uroplatus666@users.noreply.github.com>
2024-09-30 21:17:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,86 +10,86 @@ from streamlit_extras.let_it_rain import rain
 def load_model_1(model_name_1):
     rosgran = pd.read_csv(model_name_1)
     return (rosgran)
-rosgran = load_model_1('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/rosgran.csv')
+rosgran = load_model_1('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/rosgran.csv')
 
 @st.cache_resource()
 def load_model_2(model_name_2):
     all_df_copy = pd.read_csv(model_name_2)
     return (all_df_copy)
-all_df_copy = load_model_2('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/all_df_copy.csv')
+all_df_copy = load_model_2('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/all_df_copy.csv')
 
 
 @st.cache_resource()
 def load_model_3(model_name_3):
     people_zero = pd.read_csv(model_name_3)
     return (people_zero)
-people_zero = load_model_3('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/people_zero.csv')
+people_zero = load_model_3('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/people_zero.csv')
 
 
 @st.cache_resource()
 def load_model_4(model_name_4):
     places_copy = pd.read_csv(model_name_4)
     return (places_copy)
-places_copy = load_model_4('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/places_copy.csv')
+places_copy = load_model_4('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/places_copy.csv')
 
 
 @st.cache_resource()
 def load_model_5(model_name_5):
     push=pd.read_csv(model_name_5)
     return (push)
-push = load_model_5('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/push.csv')
+push = load_model_5('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/push.csv')
 
 
 @st.cache_resource()
 def load_model_6(model_name_6):
     controls_else = pd.read_csv(model_name_6)
     return (controls_else)
-controls_else = load_model_6('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/controls_else.csv')
+controls_else = load_model_6('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/controls_else.csv')
 
 
 @st.cache_resource()
 def load_model_7(model_name_7):
     places_count = pd.read_csv(model_name_7)
     return (places_count)
-places_count = load_model_7('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/places_count.csv')
+places_count = load_model_7('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/places_count.csv')
 
 
 @st.cache_resource()
 def load_model_8(model_name_8):
     rosgran_count = pd.read_csv(model_name_8)
     return (rosgran_count)
-rosgran_count = load_model_8('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/rosgran_count.csv')
+rosgran_count = load_model_8('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/rosgran_count.csv')
 
 
 @st.cache_resource()
 def load_model_9(model_name_9):
     country_else = pd.read_csv(model_name_9)
     return (country_else)
-country_else = load_model_9('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/country_else.csv')
+country_else = load_model_9('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/country_else.csv')
 
 @st.cache_resource()
 def load_model_10(model_name_10):
     Center = pd.read_csv(model_name_10)
     return (Center)
-Center = load_model_10('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/Center.csv')
+Center = load_model_10('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/Center.csv')
 
 @st.cache_resource()
 def load_model_11(model_name_11):
     ANN = pd.read_csv(model_name_11)
     return (ANN)
-ANN = load_model_11('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/ANN.csv')
+ANN = load_model_11('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/ANN.csv')
 
 @st.cache_resource()
 def load_model_12(model_name_12):
     all_df_copy_na = pd.read_csv(model_name_12)
     return (all_df_copy_na)
-all_df_copy_na = load_model_12('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/all_df_copy_na.csv')
+all_df_copy_na = load_model_12('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/all_df_copy_na.csv')
 
 @st.cache_resource()
 def load_model_13(model_name_13):
     all_df_copy_places = pd.read_csv(model_name_13)
     return (all_df_copy_places)
-all_df_copy_places = load_model_13('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/all_df_copy_places.csv')
+all_df_copy_places = load_model_13('https://raw.githubusercontent.com/uroplatus666/StreamLit/master/data/all_df_copy_places.csv')
 
 # 4. Manual item selection
 if st.session_state.get('switch_button', False):
